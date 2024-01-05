5b6ae88a28f4fc27eb07de95d9752c3d2bc07457
nancyacad01@gmail.com <nancyacad01@gmail.com>
2024-01-05 15:10:26 +0530

    file added

---
+++ Javascript.js
@@ -0,0 +1,83 @@
+console.log("Welcome to javascript")
+console.log(15)
+console.log(15.15)
+console.log({fname:"Nancy",gender:"Female"})
+console.table({fname:"Drew",gender:"Female"})
+console.log([5,10,2,24,40])
+console.table(10,15,25,20);
+console.log(true)
+
+var a=15;
+let b=10;
+const c=14;
+console.log(a);
+console.log(b);
+console.log(c);
+a=5.5;
+console.log(a);
+b=7.5;
+console.log(b);
+
+a="Nancy";
+console.log(a);
+b="Drew"
+console.log(b);
+
+a={fname:"Nancy",  Age:"24"}
+console.log(a);
+console.table(a);
+
+b={fname:"Drew",  Age:"24"}
+console.log(b);
+console.table(b);
+
+a=[11,15,24,25];
+console.log(a);
+console.table(a);
+
+b=[24,15,25,11];
+console.log(b);
+console.table(b);
+
+a=1;
+console.log(typeof a);
+
+a=2;
+console.log(typeof a);
+
+a="Drew";
+console.log(a+" : "+typeof a);
+
+a=true;
+console.log(a+" : "+typeof a);
+
+a=[15,24,10];
+console.log(typeof a);
+
+
+a={fname:"Nancy", gender:"Female"};
+console.log(typeof a);
+
+a=20;
+console.log(typeof a);
+a=a.toString();
+console.log(typeof a);
+
+a=true;
+console.log(typeof a);
+
+a=parseInt(a);
+console.log(typeof a);
+console.log(a);
+
+a=parseFloat(a);
+console.log(typeof a);
+
+
+a="25"
+d=45;
+a=a+d;
+console.log(a);
+
+
+
